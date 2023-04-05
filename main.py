9d1f5c11014c42739c3cb1e3ed37e1459a87dbf2
Jay Lee <jenlee2617@gmail.com>
2023-04-04 19:07:42 -0700

    All transcriptions done

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+import os
 import whisper
+from tqdm import tqdm
+import time
 
 model = whisper.load_model("medium.en")
-result = model.transcribe("./audio_files/Asking_A_100M_Year_Founder_For_Profitable_Business_Ideas_413_.mp4")
 
-with open('./transcriptions/business_ideas.txt', 'w') as f:
-    f.write(result["text"])
+audio_dir = "./audio_files"
+audio_files = [file_name for file_name in os.listdir(audio_dir) if file_name.endswith(".mp4")]
+
+start_time = time.time()
+for i, file_name in enumerate(tqdm(audio_files)):
+    audio_path = os.path.join(audio_dir, file_name)
+    result = model.transcribe(audio_path)
+
+    # Save transcription to file with same name as audio file
+    transcription_path = os.path.join("./transcriptions", os.path.splitext(file_name)[0] + ".txt")
+    with open(transcription_path, 'w') as f:
+        f.write(result["text"])
+
+    # Print progress indicators
+    if i % 10 == 0:
+        elapsed_time = time.time() - start_time
+        time_per_file = elapsed_time / (i + 1)
+        time_left = time_per_file * (len(audio_files) - (i + 1))
+        tqdm.write(f"Processed {i+1}/{len(audio_files)} files, elapsed time: {elapsed_time:.2f}s, time left: {time_left:.2f}s")
+
+elapsed_time = time.time() - start_time
+tqdm.write(f"Finished processing {len(audio_files)} files in {elapsed_time:.2f}s")
